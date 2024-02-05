b7f92a2b9ffa4563862f7c7050a59442d8aacaa7
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-02-05 12:53:29 +0100

    .

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
 
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
-const data = await fetchJson('https://fdnd.directus.app/items/Person/9')
-// console.log(data); // uncomment om de opgehaalde data te checken
+const data = await fetchJson('https://fdnd.directus.app/items/person/64')
+console.log(data); // uncomment om de opgehaalde data te checken
 
 // Maak een nieuwe express app aan
 const app = express()
